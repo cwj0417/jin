d2a1600f88a690ee8c56a9b8cd109411a587f979
chenwj <731028571@qq.com>
2017-06-21 18:04:40 +0800

    first blood

---
+++ jin.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global['shiny-scroll'] = global['shiny-scroll'] || {})));
+}(this, (function (exports) { 'use strict';
+
+var jin = {
+    a: "a"
+};
+
+exports.jin = jin;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs"
+
+export default {
+    entry: "./src/index.js",
+    dest: "jin.js",
+    plugins: [
+        resolve(),
+        babel({
+            exclude: "node_modules/**",
+            presets: ["es2015-rollup"]
+        }),
+        commonjs({
+            namedExports: {
+                "node_modules/lodash.throttle/index.js": ["throttle"]
+            }
+        })
+    ],
+    format: "umd",
+    moduleName: "shiny-scroll"
+};
