6446f71186f97dbe58fc9a12b50193b71bd1b597
chenwj <731028571@qq.com>
2017-06-25 20:35:03 +0800

    minor version completed

--- src/index.js
+++ src/index.js
@@ -1,36 +1,105 @@
-function assert (condition, msg) {
+function assert(condition, msg) {
     if (!condition) throw new Error(`[jin] ${msg}`)
 }
 class Color {
-    constructor ({r, g, b}) {
+    constructor({r, g, b}) {
         this.rgb = {r, g, b}
         this.hsb = this.getHSB(r, g, b)
     }
-    getHSB (r, g, bl) {
+
+    set rgb(rgb) {
+        this._rgb = rgb
+    }
+
+    set hsb(hsb) {
+        this._hsb = hsb
+    }
+
+    get rgb() {
+        return {
+            r: Math.round(this._rgb.r),
+            g: Math.round(this._rgb.g),
+            b: Math.round(this._rgb.b),
+        }
+    }
+
+    get hsb() {
+        return {
+            h: Math.round(this._hsb.h),
+            s: Math.round(this._hsb.s),
+            b: Math.round(this._hsb.b),
+        }
+    }
+
+    getHSB(r, g, bl) {
         let max = Math.max(r, g, bl)
         let min = Math.min(r, g, bl)
         let diff = max - min
-        let b = Math.round(max / 255 * 100)
+        let b = max / 255 * 100
         let s = max === 0 ? 0 : (1 - min / max) * 100
         let h
         switch (max) {
-            case min :
-                h = null
+            case min:
+                h = 0
                 break
-            case r :
+            case r:
                 h = g >= bl ? 60 * (g - bl) / diff : 60 * (g - bl) / diff + 360
                 break
-            case g :
+            case g:
                 h = 60 * (bl - r) / diff + 120
                 break
-            case bl :
+            case bl:
                 h = 60 * (r - g) / diff + 240
                 break
         }
         return {h, s, b}
     }
+
+    getRGB(h, s, br) {
+        s = s / 100
+        br = br * 255 / 100
+        let hi = Math.floor((h / 60) % 6)
+        let f = (h / 60) - hi
+        let p = br * (1 - s)
+        let q = br * (1 - f * s)
+        let t = br * (1 - (1 - f) * s)
+        let r, g, b
+        switch (hi) {
+            case 0:
+                [r, g, b] = [br, t, p]
+                break
+            case 1:
+                [r, g, b] = [q, br, p]
+                break
+            case 2:
+                [r, g, b] = [p, br, t]
+                break
+            case 3:
+                [r, g, b] = [p, q, br]
+                break
+            case 4:
+                [r, g, b] = [t, p, br]
+                break
+            case 5:
+                [r, g, b] = [br, p, q]
+                break
+        }
+        return {r, g, b}
+    }
+
+    setHSB ({h = this.hsb.h, s = this.hsb.s, b = this.hsb.b} = {}) {
+        this.rgb = this.getRGB(h, s, b)
+        return this
+    }
+
+    toString () {
+        return `#${padDigit(this.rgb.r.toString(16))}${padDigit(this.rgb.g.toString(16))}${padDigit(this.rgb.b.toString(16))}`
+    }
 }
-function parseHex (r, g, b) {
+function padDigit (origin) {
+    return origin.length === 1 ? "0" + origin : origin
+}
+function parseHex(r, g, b) {
     assert(arguments.length === 1 || arguments.length === 3, "incorrect color string")
     if (arguments.length === 3) {
         return {r, g, b}
--- test/test.js
+++ test/test.js
@@ -11,8 +11,11 @@ describe("construct", function () {
     it("rgb", function () {
         assert.deepEqual(jin(0, 255, 0).rgb, {r: 0, g: 255, b: 0})
     })
+    it("tostring", function () {
+        assert.equal(jin(0, 255, 0).toString(), "#00ff00")
+    })
 })
-describe("hsi", function () {
+describe("hsb", function () {
     it("red", function () {
         assert.deepEqual(jin(255, 0, 0).hsb, {h: 0, s: 100, b: 100})
     })
@@ -21,5 +24,19 @@ describe("hsi", function () {
     })
     it("blue", function () {
         assert.deepEqual(jin(0, 0, 255).hsb, {h: 240, s: 100, b: 100})
+    })
+    it("random1", function () {
+        assert.deepEqual(jin(52, 133, 255).hsb, {h: 216, s: 80, b: 100})
+    })
+    it("random2", function () {
+        assert.deepEqual(jin(54, 178, 92).hsb, {h: 138, s: 70, b: 70})
+    })
+})
+describe("set hsb", function () {
+    it("set h from 0 to 120 and check if it becomes green", function () {
+        assert.deepEqual(jin(255, 0, 0).setHSB({h: 120}).rgb, {r: 0, g: 255, b: 0})
+    })
+    it("random1", function () {
+        assert.deepEqual(jin(55, 31, 116).setHSB({h: 54, s: 22, b: 87}).rgb, {r: 222, g: 217, b: 173})
     })
 })
